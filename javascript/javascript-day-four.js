90a61f786ab4449610f02ea50c408810b6e23298
Firoz Akhter <firozakhter789@gmail.comgit config --list>
2025-12-06 21:41:00 +0530

    map filter reduce

--- javascript/javascript-day-four.js
+++ javascript/javascript-day-four.js
@@ -55,23 +55,114 @@
 //   console.log(key, value);
 // }
 
-const user = {
-  name: "ROHIT",
-  age: 20,
-  emailId: "example@gmail.com",
-  amount: 3400,
-  address: {
-    city: "Varanasi",
-    state: "Uttar Pradesh",
-  },
-  // greet: function () {
-  //   console.log("greetings..");
-  // },
-};
+// const user = {
+//   name: "ROHIT",
+//   age: 20,
+//   emailId: "example@gmail.com",
+//   amount: 3400,
+//   address: {
+//     city: "Varanasi",
+//     state: "Uttar Pradesh",
+//   },
+//   // greet: function () {
+//   //   console.log("greetings..");
+//   // },
+// };
 
 // const user2 = structuredClone(user);
 
 // user2.address.city = "Mau";
 
-console.log(user);
+// console.log(user);
 // console.log(user2);
+
+// forEach, filter, map, reduce
+
+// let arr = [10, 20, 30, 5, 90, 87];
+
+// let sum = 0;
+
+// arr.forEach((ele) => (sum += ele));
+
+// console.log(sum)
+
+// // filter
+
+// let res = arr.filter((ele) => ele > 25)
+
+// console.log(res)
+
+// // polyfill of filter
+
+// Array.prototype.myFilter = function(compare) {
+//     const ans = [];
+//     for(let ele of this) {
+//         if(compare(ele)) {
+//             ans.push(ele);
+//         }
+//     }
+//     return ans;
+// }
+
+// let res2 = arr.myFilter((ele) => ele > 25).sort((a, b) => a-b);
+// console.log(res2)
+
+// let arr = [10, 20, 30, 5, 90, 87];
+
+// let sum = 0;
+
+// arr.forEach((ele) => (sum += ele));
+
+// console.log(sum)
+
+// // filter
+
+// let res = arr.filter((ele) => ele > 25)
+
+// console.log(res)
+
+// // polyfill of filter
+
+// Array.prototype.myFilter = function(compare) {
+//     const ans = [];
+//     for(let ele of this) {
+//         if(compare(ele)) {
+//             ans.push(ele);
+//         }
+//     }
+//     return ans;
+// }
+
+// let res2 = arr.myFilter((ele) => ele > 25).sort((a, b) => a-b);
+// console.log(res2)
+
+const products = [
+  {
+    id: 1,
+    name: "Laptop",
+    category: "Electronics",
+    price: 1200,
+    inStock: true,
+  },
+  { id: 2, name: "Book", category: "Books", price: 30, inStock: true },
+  {
+    id: 3,
+    name: "Coffee Maker",
+    category: "Appliances",
+    price: 150,
+    inStock: false,
+  },
+  {
+    id: 4,
+    name: "Headphones",
+    category: "Electronics",
+    price: 200,
+    inStock: true,
+  },
+];
+
+const newProducts = products
+  .filter((ele) => ele.price > 50)
+  .sort((a, b) => a.price - b.price)
+  .map((ele) => ({ name: ele.name, price: ele.price }));
+console.log(newProducts);
